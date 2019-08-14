be8596dc87307070fdf312b90a9288f9fd56f48e
Chandra Gopinathan <chandrag@globalcreditresearch.com>
2019-08-14 12:06:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,12 +17,6 @@ function add2(n) {
   return n + two
   // Feel free to move things around!
 }
-
-function funkyFunction() {
-     return function() {
-     theFunk
-  }    
-}
    
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
